31e37fdce106fccfd2dccf17277ec6920fb9513c
Suriya G <suriyanarayanang16@gmail.com>
2026-02-16 14:06:33 +0530

    Fantasy Squad Builder Final

--- starting-xi.js
+++ starting-xi.js
@@ -513,7 +513,7 @@ function hideLoader() {
    SUBMISSION TIMER (XI PAGE)
 ========================= */
 
-const SUBMISSION_DEADLINE = new Date("2026-02-06T22:00:00");
+const SUBMISSION_DEADLINE = new Date("2026-02-21T11:00:00");
 let xiSubmissionLocked = false;
 
 function startXITimer() {
